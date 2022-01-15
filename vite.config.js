1af78d8fc0f5392a124775c186c935c166bbb7b4
radityaIch <radditya2678@gmail.com>
2022-01-16 01:34:50 +0800

    resolve unknown element box-icon

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,13 @@ import path from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue()],
+    plugins: [vue({
+        template: {
+            compilerOptions: {
+                isCustomElement: tag => tag === 'box-icon'
+            }
+        }
+    })],
     resolve: {
         alias: {
             "@": path.resolve(__dirname, "./src"),
